d21d94d574dfb17fecfc83970a1660d8f223ac0d
FlashBanger-NexTale <moskildmotiy2023@gmail.com>
2024-09-15 14:00:56 +0200

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,11 @@
+// script.js
+window.addEventListener('load', function() {
+    const loadingScreen = document.getElementById('loading-screen');
+    const content = document.getElementById('content');
+
+    // Hide loading screen
+    loadingScreen.style.display = 'none';
+
+    // Show content
+    content.style.display = 'block';
+});
